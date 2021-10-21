2e6a852817346349e2d187cb4626237be334c025
hyojin2689 <80669371+hyojin2689@users.noreply.github.com>
2021-10-21 15:39:42 +0900

    Update code.R

--- Organdata/code.R
+++ Organdata/code.R
@@ -4,35 +4,35 @@ head(organdata)
 library(ggplot2)
 library(dplyr)
 
-①Donors by Year (line graph)
+1.Donors by Year (line graph)
 p<-ggplot(data=organdata,mapping=aes(x=year,y=donors,color=country))
 
 p+geom_line(aes(group=country))+labs(x="Year",y="Donors",
                                           title="Donors by Year",
                                           caption="Source:organdata")
 
-②Donors by Year and Country (line graph)
+2.Donors by Year and Country (line graph)
 p<-ggplot(data=organdata,mapping=aes(x=year,y=donors))
 
 p+geom_line()+facet_wrap(~country)+labs(x="Year",y="Donors",
                                    title="Donors by Year and Country",
                                    caption="Source:organdata")
 
-③Donors by Country (box plot)
+3.Donors by Country (box plot)
 p<-ggplot(data=organdata,mapping=aes(x=reorder(country,donors,na.rm=TRUE),y=donors))
 
 p+geom_boxplot()+coord_flip()+labs(x=NULL)+labs(x=NULL,y="Donors", 
                                                 title="Donors by Country",
                                                 caption="Source:organdata")
 
-④Donors by Country and Welfare (dot plot)
+4.Donors by Country and Welfare (dot plot)
 p<-ggplot(data=organdata,mapping=aes(x=reorder(country,donors,na.rm=TRUE),y=donors,color=world))
 
 p+geom_jitter(position=position_jitter(width=0.15))+labs(x=NULL)+coord_flip()+theme(legend.position="top")+
   labs(x="Year",y="Donors",title="Donors by Country and Welfare", caption="Source:organdata")
 
 
-⑤Road accident fatalities per 100,000 population by Donors
+5.Road accident fatalities per 100,000 population by Donors
 p<-ggplot(data=organdata,mapping=aes(x=roads,y=donors))
 
 p+geom_point()+annotate(geom="rect",xmin=125,xmax=155,ymin=30,ymax=35,fill="red",alpha=0.2)+
@@ -40,7 +40,7 @@ p+geom_point()+annotate(geom="rect",xmin=125,xmax=155,ymin=30,ymax=35,fill="red"
   labs(title="Road accident fatalities per 100,000 population by Donors",
       x="Roads", y="Donors", caption="Source:organdata")
 
-⑥Visualize with summary data📊
+6.Visualize with summary data📊
 # Create summary data
 by_country<-organdata%>%group_by(consent_law,country)%>%
 summarize_if(is.numeric,funs(mean,sd),na.rm=TRUE)%>%ungroup()
